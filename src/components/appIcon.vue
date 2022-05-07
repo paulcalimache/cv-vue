e690979635d22d84bd52d33a65dc76ef326933bd
Paul Calimache <paul.calimache@viacesi.fr>
2022-05-07 21:46:44 +0200

    about-works-contact icon

--- src/components/appIcon.vue
+++ src/components/appIcon.vue
@@ -1,26 +1,44 @@
 <script>
   export default {
-    props: ['title']
-  }
+    props: ['title','icon','bgcolor'],
+  };
+
 </script>
 
 <template>
   <div class="appIcon">
+    <div class="appIcon-item" :style="{'background': bgcolor}">
+      <img :src="icon"/>
+    </div>
     <span class="appIcon-title">{{ title }}</span>
   </div>
 </template>
 
 <style scoped>
-  .appIcon {
-    width:90px;
-    height:90px;
-    border-radius: 25px;
-    background:rgba(0, 0, 0, 0.7);
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
-  }
-  .appIcon-title{
-    color:white;
-  }
+.appIcon {
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+}
+.appIcon:hover {
+  cursor:pointer;
+  transform:scale(1.1);
+}
+
+.appIcon-item {
+  width:90px;
+  height:90px;
+  border-radius: 25px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+img {
+  width:80px;
+  height:80px;
+}
+.appIcon-title{
+  color:white;
+  font-weight: bold;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,18 @@
 <script setup>
 import appIcon from '@/components/appIcon.vue';
-let contactTitle = 'Contact';
-let worksTitle = 'Works';
+let aboutTitle = 'About'; let aboutIcon = 'src/assets/Icons/paper.svg'; let aboutColor = 'rgb(38, 70, 83)';
+let worksTitle = 'Works'; let worksIcon = 'src/assets/Icons/works-box.svg'; let worksColor = 'rgb(247, 214, 224)'
+let contactTitle = 'Contact'; let contactIcon = 'src/assets/Icons/contact-mail-red.svg'; let contactColor = 'rgb(177, 221, 241)';
+let linkedinTitle = 'Linkedin';
+let githubTitle = 'Github';
+
 </script>
 
 <template>
   <div id="home">
-    <appIcon :title="contactTitle"/>
-    <appIcon :title="worksTitle"/>
-    <appIcon :title="contactTitle"/>
+    <a href="/about"><appIcon :title="aboutTitle" :icon="aboutIcon" :bgcolor="aboutColor"/></a>
+    <a href="/works"><appIcon :title="worksTitle" :icon="worksIcon" :bgcolor="worksColor"/></a>
+    <a href="/contact"><appIcon :title="contactTitle" :icon="contactIcon" :bgcolor="contactColor"/></a>
   </div>
 </template>
 
@@ -22,5 +26,13 @@ let worksTitle = 'Works';
     padding-top:40px;
     display: flex;
     justify-content: space-evenly;
+    align-items:flex-start;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  a::after {
+    text-decoration: none;
   }
 </style>
