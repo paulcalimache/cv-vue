5cbf4f4664615488cb1166fb224580020065bb33
Paul Calimache <paul.calimache@viacesi.fr>
2022-05-07 13:51:01 +0200

    my project

---
+++ src/components/appIcon.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    props: ['title']
+  }
+</script>
+
+<template>
+  <div class="appIcon">
+    <span class="appIcon-title">{{ title }}</span>
+  </div>
+</template>
+
+<style scoped>
+  .appIcon {
+    width:90px;
+    height:90px;
+    border-radius: 25px;
+    background:black;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+  .appIcon-title{
+    color:white;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import appIcon from '@/components/appIcon.vue';
+let contactTitle = 'Contact';
+let worksTitle = 'Works';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="home">
+    <appIcon :title="contactTitle"/>
+    <appIcon :title="worksTitle"/>
+    <appIcon :title="contactTitle"/>
+  </div>
 </template>
+
+<style scoped>
+  #home {
+    width:100%;
+    height: 100%;
+    background-image: url(../assets/wallpaper/POOL_DAY_QUEER_BEAR.jpg);
+    background-position: center;
+    background-size: cover;
+    padding-top:40px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+</style>
