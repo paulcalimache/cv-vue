732e6db991f18fd8cf79a96b789aa597aef0236d
Paul Calimache <paul.calimache@gmail.com>
2022-05-19 14:19:16 +0000

    work

---
+++ src/components/workComponent.vue
@@ -0,0 +1,19 @@
+<script>
+    export {
+    };
+</script>
+
+<template>
+    <div class="work">
+        <h1> workComponent works !<button @click="showDetail">+</button></h1>
+        <div v-if="showDetail" id="work-detail">
+            <p>ceci est un paragraphe</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    #work-detail {
+        display:none;
+    }
+</style>
